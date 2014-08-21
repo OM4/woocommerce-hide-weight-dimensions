187a52bf77162b7f4342080b0a6b49f29f972022
James Collins <james@om4.com.au>
2014-08-21 10:27:49 +0800

    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== WooCommerce - Hide Weight & Dimensions ===
+Contributors: jamescollins
+Tags: woocommerce, weight, dimensions, catalog, display
+Requires at least: 3.9
+Tested up to: 3.9.2
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Ensures that product weight and dimensions aren't displayed on the Additional Information tab when viewing a WooCommercce product.
+
+== Description ==
+
+WooCommerce no longer has an option to disable displaying of product weight and dimensions information.
+
+Activate this plugin, and the additional information tab will no longer include Weight and Dimensions!
+
+If you have any other suggestions for other information that could be included, please [add your suggestion via GitHub](https://github.com/OM4/woocommerce-shipping-always/issues).
+
+== Installation ==
+
+1. Download the latest stable version of the plugin from https://github.com/OM4/woocommerce-shipping-always/archive/release.zip.
+1. Install this plugin via WordPress Dashboard, Plugins, Add New, Upload.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release.
---
+++ woocommerce-hide-weight-dimensions.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: WooCommerce - Hide Weight & Dimensions
+Plugin URI: https://github.com/OM4/woocomerce-hide-weight-dimensions
+Description: Ensures that product weight and dimensions aren't displayed on the Additional Information tab when viewing a WooCommercce product.
+Version: 0.1
+Author: OM4
+Author URI: http://om4.com.au/
+Text Domain: woocomerce-hide-weight-dimensions
+Git URI: https://github.com/OM4/woocomerce-hide-weight-dimensions
+Git Branch: release
+License: GPLv2
+*/
+
+/*
+Copyright 2014 OM4 (email: info@om4.com.au    web: http://om4.com.au/)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! class_exists( 'WC_Hide_Weight_Dimensions' ) ) {
+
+	/**
+	 * This class is a singleton.
+	 *
+	 * Class WC_Hide_Weight_Dimensions
+	 */
+	class WC_Hide_Weight_Dimensions {
+
+		/**
+		 * Refers to a single instance of this class
+		 */
+		private static $instance = null;
+
+		/**
+		 * Creates or returns an instance of this class
+		 * @return WC_Hide_Weight_Dimensions A single instance of this class
+		 */
+		public static function instance() {
+			if ( null == self::$instance ) {
+				self::$instance = new self();
+			}
+			return self::$instance;
+
+		}
+
+		/**
+		 * Constructor
+		 */
+		private function __construct() {
+			add_filter( 'wc_product_enable_dimensions_display', '__return_false' );
+		}
+
+	}
+
+	WC_Hide_Weight_Dimensions::instance();
+
+}
